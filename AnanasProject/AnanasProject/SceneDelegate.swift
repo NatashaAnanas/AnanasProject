d5bc4b5ecc57b51629d8d350cd6420c318e0b6b4
Наташа Ананас <konnat7@mail.ru>
2022-10-05 19:51:00 +0300

    Шаблон Ananas Project

---
+++ AnanasProject/AnanasProject/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  AnanasProject
+//
+//  Created by Анастасия Козлова on 05.10.2022.
+//
+
+import UIKit
+/// SceneDelegate
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {}
+}
---
+++ AnanasProject/AnanasProject/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  AnanasProject
+//
+//  Created by Анастасия Козлова on 05.10.2022.
+//
+
+import UIKit
+/// ViewController
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
