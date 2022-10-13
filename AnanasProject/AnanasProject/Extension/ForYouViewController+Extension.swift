7205e3839e27d7b48aafabeaaa0ee0a7997df943
Наташа Ананас <konnat7@mail.ru>
2022-10-13 23:40:42 +0300

    add extension

---
+++ AnanasProject/AnanasProject/Extension/ForYouViewController+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  F.swift
+//  AnanasProject
+//
+//  Created by Анастасия Козлова on 13.10.2022.
+//
+
+import UIKit
+/// Расширение для ForYouViewController подписываемся на делегаты
+extension ForYouViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        self.iconImageView.image = image
+        guard let imageData = image.pngData() else { return }
+        saveUserDefaults(image: imageData)
+        dismiss(animated: true)
+    }
+}
+
+extension ForYouViewController: UINavigationControllerDelegate {
+}
